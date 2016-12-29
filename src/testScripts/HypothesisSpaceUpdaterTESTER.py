94405936a74c4aaa03ec51fd072f55c1d2bb7fb0
raboody <rosiea@Rosies-MacBook-Air.local>
2016-12-29 19:06:53 +0200

    small changes

--- src/testScripts/HypothesisSpaceUpdaterTESTER.py
+++ src/testScripts/HypothesisSpaceUpdaterTESTER.py
@@ -8,12 +8,14 @@ from HypothesisSpaceUpdater import HypothesisSpaceUpdater
 blockList = ['A','B','C','D','E']
 H = GenerateHypothesisSpace(blockList)
 
-hypothesisSpace = H.unorderedOr()
-trueHypothesis = ['B', 'E']
-example = ['AB']
-lambda_noise = .1
+hypothesisSpace = H.unorderedAnd()
+trueHypothesis = ['BE']
+#example = ['AB', 'BA']
+example = H.orderedArgs
+lambda_noise = .05
 independent = 1
 option = 0
+
 
 
 #print(hypothesisSpace, trueHypothesis, example, lambda_noise, option)
@@ -22,6 +24,22 @@ option = 0
 hUpdater = HypothesisSpaceUpdater(hypothesisSpace, trueHypothesis, example, 
 					lambda_noise, independent, option)
 
-print("Prior: ", hUpdater.hSpacePrior)
-print("Likelihood: ", hUpdater.hSpaceLikelihood)
-print("Posterior: ", hUpdater.hSpacePosterior)
+prior = hUpdater.hSpacePrior
+likelihood = hUpdater.hSpaceLikelihood
+posterior = hUpdater.hSpacePosterior
+
+
+# checking Bayes math:
+# non_normalizedPosterior = [prior*likelihood for prior, likelihood in zip(prior, likelihood)]
+# normalizedPosterior = [i/sum(non_normalizedPosterior) for i in non_normalizedPosterior]
+
+# Print prior, likelihood and posterior
+"""
+print("Prior: ", prior)f
+print("Likelihood: ", likelihood)
+print("Posterior: ", posterior)
+"""
+
+outcome = hUpdater.getOutcome(example, trueHypothesis, lambda_noise)
+print(outcome)
+
